d826cf70dbb7403714838a431bbc82b82f31177e
Fayez <fayezkhan680@gmail.com>
2023-12-25 11:50:21 +0530

    add number_crown

---
+++ number_crown.py
@@ -0,0 +1,13 @@
+for i in range(3):
+        for j in range(i+1):
+            print(j+1,end=" ")
+        for j in range(3-1-i):
+            print(" ",end=" ")
+        for j in range(3-1-i):
+      
+            print(" ",end=" ")
+      
+        for j in range(i+1):
+            print(3-j,end=" ")
+
+        print()  
